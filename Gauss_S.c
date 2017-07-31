41b6436286a2fcff0a59c5cb52b9cf0aa55eb9c5
Arya H <aryaht23@gmail.com>
2017-07-31 10:36:41 -0500

    format change

--- Gauss_S.c
+++ Gauss_S.c
@@ -1,93 +1,136 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-// created by Arya HajiTaheri
 //#pragma warning(disable:4996)
-#define tMax 10
-#define dx (M_PI/10)
-#define xMAX M_PI
-#define x0 0
+//created by arya HajiTaheri
+#define dx (M_PI/10.0)
+#define lambda 0.4
+#define x_max M_PI
+#define t_max 10.0
 #define D 1.0
-#define F 0.0
-#define n 1.0
-
-#define lambda .4
+#define N 1.0
 #define dt (lambda*dx*dx/D)
-//function declaration
-double U0(double);
-double exact(double, double);
-void Gauss_S_N();
+#define iteration_K 5000000
 
+#define SIZE_X (int)(ceil(x_max/dx))
+#define SIZE_T (int)(ceil(t_max/dt))
+
+int F(double, double);
+double exact(double, double);
+void Gauss_S();
+double error(double, double);
+void print(double U[SIZE_T + 1][SIZE_X + 1]);
 int main(void)
 {
-    Gauss_S_N();
+    Gauss_S();
     return 0;
 }
 
-void Gauss_S_N()
+int F(double t, double x)
 {
-    // init
-    int k;
-    printf("Enter k:\n");
-    scanf("%d", &k);
-    int N = (int)((xMAX - x0) / dx), M = (int)(tMax / dt);
-    double u[M + 1][N + 1], error = 0.0;
-
-
-    int i, j, t;
-    FILE *error_f = fopen("Error.csv", "w"), *estimate_f= fopen("Estimate.csv", "w"), *exact_f= fopen("Exact.csv", "w");
-    for (t = 0; t <= M; t++)
-    {
-        for (i = 1; i<N; i++)
-        {
-            u[t][i] = U0(i*dx);
-        }
-    }
-    for (i = 0; i <= M; i++)
-    {
-        u[i][0] = 0;
-        u[i][N] = 0;
-    }
-
-    for (i = 0; i <= M; i++)
-    {
-        if (u[i][j] == 0) // because error would be infinity (0/0)
-        {
-            error = 0;
-        }
-
-        printf("Estimate: ");
-        for (j = 0; j <= N; j++)
-        {
-            printf("%.8lf ", u[i][j]);
-            fprintf(estimate_f, "%lf, ", u[i][j]);
-            fprintf(exact_f, "%lf, ", exact(j*dx, i*dt));error = fabs((u[i][j] - exact(j*dx, i*dt)) / u[i][j]) * 100;
-            fprintf(error_f, "%lf, ", error);
-        }
-        printf("\n\nExact:");
-        for (j = 0; j <= N; j++)
-        {
-            printf("%.8lf ", exact(j*dx, i*dt));
-        }
-        printf("\n");
-        printf("\n");
-        fprintf(estimate_f, "\n");
-        fprintf(exact_f, "\n");
-        fprintf(error_f, "\n");
-
-    }
-    printf("\n\n\a");
-
-    fclose(exact_f);
-    fclose(estimate_f);
-    fclose(error_f);
-}
-double U0(double x)
-{
-    return  sin(n*x);
-}
-double exact(double x, double t)
-{
-    return exp(-n*n*t)*sin(n*x);
+    return 0;
 }
 
+double exact(double t, double x)
+{
+    return exp(-N*N*t)*sin(N*x);
+}
+
+double error(double exact, double approx)//calculate error %
+{
+    return (fabs(approx - exact) / fabs(approx)) * 100;
+}
+void print(double U[SIZE_T + 1][SIZE_X + 1])
+{
+    int n, j;
+
+    double exact_value = 0, error_value = 0;
+    FILE *u, *x, *t, *exact_graph, *error_graph;
+    u = fopen("u.csv", "w"), x = fopen("x.csv", "w"), t = fopen("t.csv", "w"),
+    exact_graph = fopen("exact.csv", "w"), error_graph = fopen("error.csv", "w");
+
+    for (n = 0; n <= SIZE_T; n++)
+    {
+        for (j = 0; j <= SIZE_X; j++)
+        {
+            exact_value = exact(n*dt, j*dx);
+            error_value = error(exact_value, U[n][j]);
+            if (j == 0 || j == SIZE_X) // cannot divide 0/0
+            {
+                error_value = 0.0;
+            }
+            printf("Estimate: %.15lf\tExact: %.15lf\tError: %.15lf%%\n", U[n][j], exact_value, error_value);
+            fprintf(u, "%lf\n", U[n][j]);
+            fprintf(x, "%d,", j);
+            fprintf(t, "%d\n", n);
+            fprintf(exact_graph, "%lf\n", exact_value);
+            fprintf(error_graph, "%lf\n", error_value);
+        }
+        printf("\n\n");
+    }
+    fclose(u), fclose(x), fclose(t),
+           fclose(exact_graph), fclose(error_graph);
+}
+void Gauss_S()
+{
+    int n, k, i, t, x, tol;
+
+    double u[SIZE_T + 1][SIZE_X + 1];
+
+    //calculating u[][]
+    for (k = 0; k <= SIZE_T; k++)
+    {
+        for (i = 0; i <= SIZE_X; i++)
+        {
+            u[k][i] = sin(i*dx*N);
+        }
+    }
+    //set right/left bounds
+    for (n = 0; n <= SIZE_T; n++)
+    {
+        u[n][0] = 0;
+        u[n][SIZE_X] = 0;
+    }
+
+    //init values
+    double a = 1.0 + 2.0*lambda, b = -lambda, c = -lambda,
+           temp_u[SIZE_T][SIZE_X], tolerance = 0, bool_last_iteration = 0, abort_program = 0;
+    for (x = 0; x <= SIZE_X; x++)
+    {
+        temp_u[0][x] = u[0][x]; // have a u to compare tolerance to
+    }
+
+    for (t = 1; t <= SIZE_T; t++) // follow t->k->x loop for accurate process.
+    {
+        for (k = 1; k<iteration_K; k++)
+        {
+            bool_last_iteration = 0;
+            abort_program = 0;
+            for (x = 1; x<SIZE_X; x++)
+            {
+                u[t][x] = (u[t - 1][x] - b*u[t][x - 1] - c*u[t][x + 1]) / a;
+                temp_u[k][x] = u[t][x];
+            }
+            for (tol = 1; tol<SIZE_X; tol++)
+            {
+                tolerance = fabs(u[t][tol] - temp_u[k - 1][tol]);
+                if (tolerance<0.0000000001) // if tolerance is getting smaller than this, abort.
+                {
+
+                    abort_program++;
+                    if (abort_program == SIZE_X - 1)
+                    {
+                        bool_last_iteration = 1;
+                        break;
+                    }
+                }
+            }
+            if (bool_last_iteration == 1)
+            {
+                break;
+            }
+        }
+    }
+    printf("Done! Now printing...\n\a");
+    print(u);
+}
